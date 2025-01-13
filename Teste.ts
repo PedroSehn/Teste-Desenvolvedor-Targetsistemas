869f100ef254ce4c0203326ba64e7ff3e7f409c0
PedroSehn <pedrorsehn@hotmail.com>
2025-01-13 16:25:42 -0300

    adiciona o resto dos requisitos, e funçoes que executam os requisitos

--- Teste.ts
+++ Teste.ts
@@ -60,3 +60,60 @@ function analisarFaturamento(faturamento: Faturamento): void {
   console.log(`Maior faturamento: R$${maiorValor}`);
   console.log(`Dias com faturamento acima da média: ${diasAcimaMedia}`);
 }
+
+// 4) Calculo percentual de representação
+function calcularPercentualPorEstado(): void {
+  const faturamentoEstados = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+  };
+
+  const totalFaturamento = Object.values(faturamentoEstados).reduce((acc, curr) => acc + curr, 0);
+
+  for (let estado in faturamentoEstados) {
+    const percentual = (faturamentoEstados[estado] / totalFaturamento) * 100;
+    console.log(`Percentual de ${estado}: ${percentual.toFixed(2)}%`);
+  }
+}
+
+// 5) Inverter os caracteres de uma string
+function inverterString(s: string): string {
+  let sInvertida = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    sInvertida += s[i];
+  }
+  return sInvertida;
+}
+
+// Função para rodar todas as questões
+function executar() {
+  // 1) Calcular a soma
+  calcularSoma();
+
+  // 2) Verificar número na sequência de Fibonacci
+  const numero = 21; // Exemplo de número a ser testado
+  console.log(verificaFibonacci(numero));
+
+  // 3) Analisar faturamento
+  const faturamento = {
+    "SP": [67450, 68000, 71000, 69000, 70000],
+    "RJ": [36000, 35000, 37000, 36500, 36800],
+    "MG": [29000, 28000, 30000, 31000, 29500],
+    "ES": [27000, 27500, 26700, 26000, 25000],
+    "Outros": [19000, 18000, 20000, 19000, 21000]
+  };
+  analisarFaturamento(faturamento);
+
+  // 4) Calcular percentual de faturamento por estado
+  calcularPercentualPorEstado();
+
+  // 5) Inverter uma string
+  const texto = "Exemplo de string"; // Exemplo de string
+  console.log(`String invertida: ${inverterString(texto)}`);
+}
+
+
+executar();
